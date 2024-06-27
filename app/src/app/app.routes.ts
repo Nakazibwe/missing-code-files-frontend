d98fbb348b0f4f2e075e0d7da5243d40a4f51b6e
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2024-06-27 15:25:08 +0300

    Adding routes to routes array

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from './components/login/login.component';
+import {TwofactorComponent} from './components/twofactor/twofactor.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'auth/login', component: LoginComponent },
+    { path: 'auth/verify', component: TwofactorComponent },
+    { path: '', redirectTo: '/auth/login', pathMatch: 'full' }, // Redirect to login by default
+    { path: '**', redirectTo: '/auth/login' } // Redirect unknown routes to login
+];
+
