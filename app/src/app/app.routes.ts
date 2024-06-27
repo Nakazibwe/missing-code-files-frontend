6640a4e4ebaf18b43740e93ed04756fb74e6163f
Grace Nakazibwe <nakazibwe19grace@gmail.com>
2024-06-27 16:56:28 +0300

    Email alert route added

--- app/src/app/app.routes.ts
+++ app/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from './components/login/login.component';
 import {TwofactorComponent} from './components/twofactor/twofactor.component';
+import {EmailalertComponent} from './components/emailalert/emailalert.component';
 
 export const routes: Routes = [
     { path: 'auth/login', component: LoginComponent },
     { path: 'auth/verify', component: TwofactorComponent },
+    { path: 'email-alert', component: EmailalertComponent },
     { path: '', redirectTo: '/auth/login', pathMatch: 'full' }, // Redirect to login by default
     { path: '**', redirectTo: '/auth/login' } // Redirect unknown routes to login
 ];
